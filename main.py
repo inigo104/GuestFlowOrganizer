09006388fa32d4c89a520399d131fae1eda3db51
inigo104 <inigo104@gmail.com>
2023-04-08 19:56:20 +0200

    Implmetar FastAPI

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import json
 from pyairtable import Table
 import pandas as pd
 from datetime import datetime
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
@@ -15,16 +17,13 @@ TABLE_NAME = os.getenv('TABLE_NAME')
 VIEW_NAME_LLEGADAS = os.getenv('VIEW_NAME_LLEGADAS')
 VIEW_NAME_SALIDAS = os.getenv('VIEW_NAME_SALIDAS')
 
-# Cargar las configutaciones desde el archivo config.json
+# Cargar las configuraciones desde el archivo config.json
 def load_config(file_path):
     with open(file_path, 'r') as f:
         config = json.load(f)
     return config
 
 config = load_config('config.json')
-
-# ... (resto del código)
-
 
 def get_airtable_records(api_key, base_id, table_name, view_name):
     table = Table(api_key, base_id, table_name)
@@ -46,8 +45,24 @@ def process_dataframe(df, view_type, config):
     df['Origen del dato'] = view_type
     return df
 
+app = FastAPI()
 
-def  main():
+def generate_html_table(df):
+    table_rows = []
+    for _, row in df.iterrows():
+        row_html = ''.join([f'<td>{value}</td>' for value in row])
+        table_rows.append(f'<tr>{row_html}</tr>')
+
+    headers_html = ''.join([f'<th>{header}</th>' for header in df.columns])
+    table_headers = f'<tr>{headers_html}</tr>'
+
+    table_html = f'<table><thead>{table_headers}</thead><tbody>{" ".join(table_rows)}</tbody></table>'
+    return table_html
+
+column_order = ['Fecha', 'Hora', 'Origen del dato', 'Origen', 'Referencia', 'H. salida', 'Hotel', 'Nombre invitado', 'Acompañantes', 'Sección']
+
+@app.get('/', response_class=HTMLResponse)
+async def get_table():
     records_llegadas = get_airtable_records(API_KEY, BASE_ID, TABLE_NAME, VIEW_NAME_LLEGADAS)
     records_salidas = get_airtable_records(API_KEY, BASE_ID, TABLE_NAME, VIEW_NAME_SALIDAS)
 
@@ -57,17 +72,16 @@ def  main():
     llegadas_df_processed = process_dataframe(llegadas_df, 'llegada', config)
     salidas_df_processed = process_dataframe(salidas_df, 'salida', config)
 
-    # Combinar los DataFrames
     combined_df = pd.concat([llegadas_df_processed, salidas_df_processed], ignore_index=True)
-
-    # Reordenar columnas si es necesario
-    column_order = ['Fecha','Hora', 'Origen del dato', 'Origen', 'Referencia', 'H. salida', 'Hotel', 'Nombre invitado', 'Acompañantes', 'Sección']
     existing_columns = [col for col in column_order if col in combined_df.columns]
     combined_df = combined_df[existing_columns]
 
-    # Guardar el DataFrame combinado en un archivo CSV
-    combined_df.to_csv('combined_data.csv', sep=';', decimal=',', index=False, encoding='utf-8-sig')
+    combined_df['Fecha'] = pd.to_datetime(combined_df['Fecha'])
+    combined_df = combined_df.sort_values(by=['Fecha', 'Hora'])
+    combined_df['Fecha'] = combined_df['Fecha'].dt.strftime('%Y-%m-%d')
 
+    # Reemplazar NaN con una cadena vacía
+    combined_df = combined_df.fillna('')
 
-if __name__ == '__main__':
-    main()
+    table_html = generate_html_table(combined_df)
+    return f'<html><head><style>table {{border-collapse: collapse;}} th, td {{border: 1px solid black; padding: 8px;}} th {{background-color: #f2f2f2;}}</style></head><body>{table_html}</body></html>'
