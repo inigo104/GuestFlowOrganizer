45a3a7dede0e829596194ee9df1338c0a1781168
inigo104 <inigo104@gmail.com>
2023-04-08 17:19:31 +0200

    Combinar ambas vistas

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import json
 
 from pyairtable import Table
 import pandas as pd
@@ -14,6 +15,14 @@ TABLE_NAME = os.getenv('TABLE_NAME')
 VIEW_NAME_LLEGADAS = os.getenv('VIEW_NAME_LLEGADAS')
 VIEW_NAME_SALIDAS = os.getenv('VIEW_NAME_SALIDAS')
 
+# Cargar las configutaciones desde el archivo config.json
+def load_config(file_path):
+    with open(file_path, 'r') as f:
+        config = json.load(f)
+    return config
+
+config = load_config('config.json')
+
 # ... (resto del código)
 
 
@@ -21,15 +30,43 @@ def get_airtable_records(api_key, base_id, table_name, view_name):
     table = Table(api_key, base_id, table_name)
     return table.all(view=view_name)
 
-def to_data_frame_to_csv(records, filename):
-    df = pd.DataFrame.from_records((r['fields'] for r in records))
-    df.to_csv(f'{filename}_file.csv', sep=';', decimal=',', index=False, encoding='utf-8-sig')
+def to_data_frame(records):
+    return pd.DataFrame.from_records((r['fields'] for r in records))
+
+def process_dataframe(df, view_type, config):
+    view_fields = config['view_fields'][view_type]
+    df = df.rename(columns={
+        view_fields['Hora']: 'Hora',
+        view_fields['Origen']: 'Origen',
+        view_fields['Referencia']: 'Referencia',
+        view_fields['H. salida']: 'H. salida',
+        view_fields['Fecha']: 'Fecha',
+        view_fields['Nombre invitado']: 'Nombre invitado'
+    })
+    df['Origen del dato'] = view_type
+    return df
+
 
 def  main():
     records_llegadas = get_airtable_records(API_KEY, BASE_ID, TABLE_NAME, VIEW_NAME_LLEGADAS)
     records_salidas = get_airtable_records(API_KEY, BASE_ID, TABLE_NAME, VIEW_NAME_SALIDAS)
-    to_data_frame_to_csv(records_llegadas, 'llegadas')
-    to_data_frame_to_csv(records_salidas, 'salidas')
+
+    llegadas_df = to_data_frame(records_llegadas)
+    salidas_df = to_data_frame(records_salidas)
+
+    llegadas_df_processed = process_dataframe(llegadas_df, 'llegada', config)
+    salidas_df_processed = process_dataframe(salidas_df, 'salida', config)
+
+    # Combinar los DataFrames
+    combined_df = pd.concat([llegadas_df_processed, salidas_df_processed], ignore_index=True)
+
+    # Reordenar columnas si es necesario
+    column_order = ['Fecha','Hora', 'Origen del dato', 'Origen', 'Referencia', 'H. salida', 'Hotel', 'Nombre invitado', 'Acompañantes', 'Sección']
+    existing_columns = [col for col in column_order if col in combined_df.columns]
+    combined_df = combined_df[existing_columns]
+
+    # Guardar el DataFrame combinado en un archivo CSV
+    combined_df.to_csv('combined_data.csv', sep=';', decimal=',', index=False, encoding='utf-8-sig')
 
 
 if __name__ == '__main__':
